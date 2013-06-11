df7e116dd3cf486868c4165937b2054718db08ca
Aaron Gough <aaron@aarongough.com>
2013-06-11 15:00:19 -0400

    Fixed bug in ImageResize that caused images not to be resized. *sigh*

--- lib/potion/extensions/photo_helper.rb
+++ lib/potion/extensions/photo_helper.rb
@@ -1,12 +1,12 @@
 module Potion::Helpers
-  def photo(identifier, alt_text = "")
+  def photo(identifier, attributes = {})
     raise "\n\nERROR: The 'photo' helper only works for posts.\n\n" unless self.is_a?(Potion::Post)
     
     extensions = [".jpg", ".jpeg", ".png", ".gif"]
     photos = @static_files.select {|file| extensions.include?(File.extname(file.path).downcase)}
     
     if identifier.is_a?(Integer)
-      photo = photos.sort {|a,b| a.path <=> b.path }[identifier]
+      photo = photos.sort {|a,b| a.path <=> b.path }[identifier - 1]
     else
       candidates = photos.select{|photo| File.split(photo.path)[1].include?(identifier)}
       raise "\n\nERROR: '#{identifier}' could refer to more than one photo: #{candidates.map{|x|File.split(x.path)[1]}.inspect} in #{self.path}\n\n" unless candidates.length == 1
@@ -14,6 +14,8 @@ module Potion::Helpers
     end
     
     raise "\n\nERROR: No photo matching '#{identifier}' was found in: #{@path}\n\n" if photo.nil?
-    "<img alt=\"#{alt_text}\" src=\"#{photo.relative_output_path}\"/>"
+    attributes_string = ""
+    attributes.each_pair {|k,v| attributes_string << "#{k}=\"#{v}\" "}
+    "<img #{attributes_string} src=\"#{photo.relative_output_path}\"/>"
   end
 end
--- lib/potion/extensions/photo_resize.rb
+++ lib/potion/extensions/photo_resize.rb
@@ -2,16 +2,14 @@ require 'mini_magick'
 
 class PhotoResize
   def process(item)
-    return unless item.is_a?(Potion::Post)
+    return unless item.path.include?("_posts")
     return unless item.site.config["photo_resize"]["enabled"]
     extensions = [".jpg", ".jpeg", ".gif", ".png"]    
 
-    item.static_files.each do |file|
-      next unless extensions.include?(File.extname(file.output_path).downcase)
-      image = MiniMagick::Image.read(file.content)
-      image.resize(item.site.config["photo_resize"]["size"])
-      file.content = image.to_blob
-    end
+    return unless extensions.include?(File.extname(item.output_path).downcase)
+    image = MiniMagick::Image.read(item.content)
+    image.resize(item.site.config["photo_resize"]["size"])
+    item.content = image.to_blob
   end
 end
 
